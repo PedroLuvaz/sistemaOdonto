0f0f050b7c0801824ead8ff10a056f624b3480e3
Pedro Andrade <113270952+PedroLuvaz@users.noreply.github.com>
2025-02-27 22:47:07 -0300

    Primeira commit, estruturação e mapeamento de entidades

---
+++ Controllers/Context/SistemContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Controllers.Map;
+
+namespace Controllers.Context
+{
+    class SistemContext : DbContext
+    {
+        public SistemContext() : base("Server=PEDRO\\SQLEXPRESS; Database=odonto; Integrated Security=True;")
+        {
+        
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<SistemContext>(null);
+            modelBuilder.Configurations.Add(new DentistaMap());
+            modelBuilder.Configurations.Add(new PacienteMap());
+            modelBuilder.Configurations.Add(new ConsultaMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SistemaOdonto/Form1.cs
@@ -0,0 +1,10 @@
+namespace SistemaOdonto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
